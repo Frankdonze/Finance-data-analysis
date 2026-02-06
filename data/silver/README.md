7683f15f21f18b04a9c3da4d1ebede0dfd86e8db
frank donze <ftd0001@auburn.edu>
2026-02-06 18:52:03 +0000

    progress

--- data/silver/README.md
+++
@@ -1,1 +0,0 @@
-This folder is used to store cleaned data in the parquet format. Files in this folder are hidden.
---
+++ etl/explore.py
@@ -0,0 +1,15 @@
+import requests
+import os
+import json
+
+api_key = os.getenv("APIKEY")
+tickers = ["AAPL", "NVDA", "TSLA", "AMZN"]
+for tick in tickers:
+    url = "https://financialmodelingprep.com/stable/historical-price-eod/light?symbol=" + tick + "&apikey=" + api_key
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        with open("/home/frank/fin-da/data/bronze/" + tick + ".json", "w") as f:
+            json.dump(response.json(), f, indent=4)
+    else:
+        print("Error:", response.status_code)
